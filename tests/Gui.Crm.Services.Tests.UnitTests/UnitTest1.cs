0d460483a74248f5e703d12298f4db5169e8a6f0
Jose Alonso <jarommat@gmail.com>
2021-04-17 14:49:33 -0500

    Adding Unit Test Project. Initial

---
+++ tests/Gui.Crm.Services.Tests.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Gui.Crm.Services.Tests.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
